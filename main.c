4844990f38a5c40cf68ef23176336118b0f0aea5
jiminhong111 <70951888+jiminhong111@users.noreply.github.com>
2020-10-15 16:04:50 +0900

    07-4

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	
-	int flag = 1;
-	int y;
-	
-	while(flag != 0)
+void f(void);
+int i;
 
-	{
-		y = 3;
-		flag =0;
+int main(int argc, char *argv[]){
+	
+	for(i=0;i<5;i++){
+		f();
 	}
-	y = 4;
 	
 	return 0;
+}
+
+void f(void)
+{
+	for (i=0;i<10;i++)
+		printf("#");
 }
